76232b55b01b67c66de04eafb62b8c36153da131
Angga Irham Stianto <angga@usm.ac.id>
2023-10-16 09:18:16 +0700

    update

--- app/src/main/java/com/example/droidlogin94/LoginActivity.kt
+++ app/src/main/java/com/example/droidlogin94/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.droidlogin94
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var txtRegister: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        txtRegister = findViewById(R.id.txt_register)
+        txtRegister.setOnClickListener {
+            val moveIntent = Intent(this@LoginActivity,RegisterActivity::class.java)
+            startActivity(moveIntent)
+        }
     }
 }
--- app/src/main/java/com/example/droidlogin94/RegisterActivity.kt
+++ app/src/main/java/com/example/droidlogin94/RegisterActivity.kt
@@ -1,11 +1,18 @@
 package com.example.droidlogin94
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var txtLogin: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        txtLogin = findViewById(R.id.txt_login)
+        txtLogin.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
